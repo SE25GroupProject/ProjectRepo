c38bbc17c61486d5b770cd16cdddcdae673abe79
Camille <mearjoneskids@gmail.com>
2025-01-08 20:00:58 +0000

    Added Python file
    Made the python file mentioned in task 5 of the homework. Then added error from task 6.

---
+++ myfile.py
@@ -0,0 +1,13 @@
+a = 2
+b = 3
+c = 4
+
+print("a + b = " + str(a + b))
+print("b + c = " + str(b + c))
+print("a + c = " + str(a + c))
+
+print("Total: " + str(a + b + c))
+
+print("This totally works!")
+
+invalid = {"value" 6,}
